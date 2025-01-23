ca81ddbee1c290cfc48683656cba0c0442500114
Guksu <yung3152@daum.net>
2025-01-23 15:00:25 +0900

    chore : 초기세팅

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "국수의 개발 블로그",
+  description:
+    "프론트엔드 개발자 국수의 개발 블로그입니다. 개발하면서 발생한 이슈나 이야깃거리를 정리하고 있습니다.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={`antialiased`}>{children}</body>
+    </html>
+  );
+}
