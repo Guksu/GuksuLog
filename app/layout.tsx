7ebae3da3ec59ca4d734bd7b9dfe18897e81daed
Guksu <yung3152@daum.net>
2025-01-24 23:10:41 +0900

    chore : @vercel/analytics/react 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/react";
 export const metadata: Metadata = {
   title: "국수의 개발 블로그",
   description:
@@ -29,6 +29,7 @@ export default function RootLayout({
     <html lang="ko">
       <body className="font-normal antialiased max-w-screen-xl mx-auto">
         {children}
+        <Analytics />
       </body>
     </html>
   );
